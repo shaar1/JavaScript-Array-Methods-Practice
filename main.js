bc8c2360c35a5b3a2d64044ca6fbd41b568fa337
Abdallah <abdallahchaar90@gmail.comh>
2021-01-10 15:53:55 +0100

    träning om olika exampel om array

---
+++ main.js
@@ -0,0 +1,11 @@
+/*global console, alert, propt*/
+
+var myMsg = "Welcome To that websait";
+
+if (Array.isArray(myMsg)){
+    
+    console.log("Yes This Is Array");
+
+}else{
+    console.log("No This Is Not Array");
+}
