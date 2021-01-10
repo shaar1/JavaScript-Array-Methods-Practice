03c1393c5a4c381ac47578f116a3664c42d34cc4
Abdallah <abdallahchaar90@gmail.comh>
2021-01-10 15:56:12 +0100

    exampel som är array

--- main.js
+++ main.js
@@ -1,11 +1,29 @@
 /*global console, alert, propt*/
-
+/*
 var myMsg = "Welcome To that websait";
 
 if (Array.isArray(myMsg)){
-    
+
     console.log("Yes This Is Array");
 
 }else{
+    console.log("No This Is Not Array");
+}
+*/
+
+var myMsg = [
+
+    "Msg 1",
+    "Msg 2",
+    "Msg 3",
+    "Msg 4"
+
+];
+
+if (Array.isArray(myMsg)) {
+
+    console.log("Yes This Is Array");
+
+} else {
     console.log("No This Is Not Array");
 }
